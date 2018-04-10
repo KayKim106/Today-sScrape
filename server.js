23aefccc9bc9d2e04cf683464d25c60bdd552156
kaykim106@gmail.com <kaykim106@gmail.com>
2018-04-10 11:21:27 -0400

    adds scraping js and database creation

---
+++ server.js
@@ -0,0 +1,60 @@
+const express =require("express");
+const request =require("request");
+const cheerio = require("cheerio");
+const mongojs = require("mongojs");
+
+var app = express();
+
+
+var databaseurl = "todayscrape";
+var collections = ["scrapedData"];
+var db = mongojs(databaseurl,collections);
+
+db.on("error",function(error){
+	console.log("Database connection error:",error);
+});
+//first page of the scraping
+app.get("/",function(req,res){
+	res.send("Welcome to the first page");
+});
+//getting all scraped data by json format
+app.get("/all",function(req,res){
+	db.scrapedData.find({},function(err,data){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.json(data);
+		}
+	});
+});
+//scraping data from newyork times
+app.get("/scrape",function(req,res){
+	request("https://www.nytimes.com/",function(err,response,html){
+		var $=cheerio.load(html);
+		 $(".story-heading").each(function(i, element) {
+      // Save the text and href of each link enclosed in the current element
+      var title = $(element).children("a").text();
+      var link = $(element).children("a").attr("href");
+      	//if title and link is scrapping correctly, then run the below code
+		if(title&&link){
+			db.scrapedData.insert({
+				title:title,
+				link:link
+			},
+			function(err,inserted){
+				if(err){
+					console.log(err);
+				}
+				else{
+					console.log(inserted)
+				}
+			});
+		}
+		});
+	});
+res.send("Scraped!");
+});
+app.listen(3000,function(){
+	console.log("App listening Port : 3000");
+});
